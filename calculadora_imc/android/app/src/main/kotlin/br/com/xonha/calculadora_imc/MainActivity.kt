67cd13ebccddf6a12d83a3173c66ac6a7ed25849
Xonha <xonha@pm.me>
2020-08-31 11:43:26 -0300

    Adicionado primeiros programas

---
+++ calculadora_imc/android/app/src/main/kotlin/br/com/xonha/calculadora_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.xonha.calculadora_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ conversor_de_moedas/android/app/src/main/kotlin/br/com/xonha/conversor_de_moedas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.xonha.conversor_de_moedas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
